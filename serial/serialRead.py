ff07f063ffa9587b4d5b9f1c6b0335cfcb730680
johnhopkins <johnhopkins@gmx.co.uk>
2016-01-19 19:06:53 +0000

    changed info in serialRead

--- serial/serialRead.py
+++ serial/serialRead.py
@@ -1,14 +1,19 @@
 import serial, datetime
 from pymongo import MongoClient
 
+''' To find the serial port you require type ls /dev/tty.* into your terminal 
+	which will give you a list of all serial ports in use'''
+
 SERIALPORT = "/dev/tty.usbmodemFA1311"
 BAUDRATE = 9600
 
 ser = serial.Serial(SERIALPORT, BAUDRATE)
 db = MongoClient('192.168.0.3')['homeautomation']['temperatures']
 
-''' Following loop reads serial port for incoming temperature data. Then updates the current temperature and inserts
-the data into our mongo database along with date and time  '''
+''' Following loop reads serial port for incoming temperature data.
+	Then updates the current temperature and inserts
+	the data into our mongo database along with date and time  '''
+
 while True:
     data_raw = float(ser.readline().strip())
     db.update({'_id':'current_temp'},{'datetime':datetime.datetime.now(),'room':'attic','temp':data_raw}, upsert=True)
